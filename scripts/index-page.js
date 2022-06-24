885f847305dbe033da5b9faf779b5a2d850f3e2d
Kulani <bastion.rotor-0k@icloud.com>
2022-06-23 23:26:25 -0600

    Refactor index-page.js file to retrieve comment data from the API.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,31 +1,41 @@
 const apiKey = '61c0e2cf-298d-46c5-8566-447849b3e892'; 
 
-const currentComments = [
-    {
-        name: 'Connor Walton',
-        timestamp: new Date(2021, 1, 17),
-        text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' 
-    },
+// const currentComments = [
+//     {
+//         name: 'Connor Walton',
+//         timestamp: new Date(2021, 1, 17),
+//         text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' 
+//     },
 
-    {
-        name: 'Emilie Beach',
-        timestamp: new Date(2021, 0, 9),
-        text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
-    },
+//     {
+//         name: 'Emilie Beach',
+//         timestamp: new Date(2021, 0, 9),
+//         text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
+//     },
 
-    {
-        name: 'Miles Acosta',
-        timestamp: new Date(2020, 11, 20),
-        text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." 
-    }
-];
+//     {
+//         name: 'Miles Acosta',
+//         timestamp: new Date(2020, 11, 20),
+//         text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." 
+//     }
+// ];
 
 const formElement = document.querySelector('#commentForm');
 const commentsContainer = document.querySelector('#currentCommentsContainer');
 
-currentComments.forEach((currentComment) => {
-    displayComment(currentComment);
- });
+// currentComments.forEach((currentComment) => {
+//     displayComment(currentComment);
+//  });
+
+axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
+    .then((responses) => {
+        // responses.forEach((response) => {
+        //     displayComment(response);
+        // });
+        responses.data.forEach((response) => {
+            displayComment(response);
+        });
+    });
 
 formElement.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -33,7 +43,7 @@ formElement.addEventListener('submit', function(e) {
     const newComment = {};
     newComment.name = e.target.fullName.value; 
     newComment.timestamp = new Date();
-    newComment.text = e.target.comment.value;
+    newComment.comment = e.target.comment.value;
 
     currentComments.push(newComment);
 
@@ -50,7 +60,7 @@ formElement.addEventListener('submit', function(e) {
     document.querySelector('#commentForm').reset();
 });
 
-function displayComment(comment) {
+function displayComment(commentInfo) {
 
     //Article element
     const commentArticle = document.createElement('article'); 
@@ -74,28 +84,30 @@ function displayComment(comment) {
 
             //Name
             const commentName = document.createElement('p');
-            commentName.innerText = comment.name;
+            commentName.innerText = commentInfo.name;
             commentNameDateContainer.appendChild(commentName);
 
             //Date
             const commentDate = document.createElement('p');
-            const monthAdjust = comment.timestamp.getMonth()+1;
+            const date = new Date(commentInfo.timestamp);
+            const monthAdjust = date.getMonth()+1;
+            const dayAdjust = date.getDate()+1;
 
-            if (comment.timestamp.getMonth() < 10 && comment.timestamp.getDate() < 10) {
-                commentDate.innerText = '0' + monthAdjust + '/' + '0' + comment.timestamp.getDate() + '/' + comment.timestamp.getFullYear();
-            } else if (comment.timestamp.getMonth() < 10 && comment.timestamp.getDate() > 9) {
-                commentDate.innerText = '0' + monthAdjust + '/' + comment.timestamp.getDate() + '/' + comment.timestamp.getFullYear();
-            } else if (comment.timestamp.getMonth() > 9 && comment.timestamp.getDate() < 10) {
-                commentDate.innerText = monthAdjust + '/' + '0' + comment.timestamp.getDate() + '/' + comment.timestamp.getFullYear();
+            if (date.getMonth() < 10 && date.getDate() < 10) {
+                commentDate.innerText = '0' + monthAdjust + '/' + '0' + dayAdjust + '/' + date.getFullYear();
+            } else if (date.getMonth() < 10 && date.getDate() > 9) {
+                commentDate.innerText = '0' + monthAdjust + '/' + dayAdjust + '/' + date.getFullYear();
+            } else if (date.getMonth() > 9 && date.getDate() < 10) {
+                commentDate.innerText = monthAdjust + '/' + '0' + dayAdjust + '/' + date.getFullYear();
             } else {
-                commentDate.innerText = monthAdjust + '/' + comment.timestamp.getDate() + '/' + comment.timestamp.getFullYear();
+                commentDate.innerText = monthAdjust + '/' + dayAdjust + '/' + date.getFullYear();
             };
 
             commentNameDateContainer.appendChild(commentDate);
 
         //Comment text
         const commentText = document.createElement('p'); 
-        commentText.innerText = comment.text;
+        commentText.innerText = commentInfo.comment;
         commentTextContainer.appendChild(commentText);
 
 
